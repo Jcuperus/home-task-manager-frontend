ddebc51aaec2ff33f62f95cba55bfb83ef2012f1
Jaep Cuperus <jaepcuperus@hotmail.com>
2020-05-28 21:05:34 +0200

    Sidebar refactor, responsiveness improvements

--- src/app/common-components/common-components.module.ts
+++ src/app/common-components/common-components.module.ts
@@ -3,11 +3,15 @@ import { CheckboxGroupComponent } from './checkbox-group.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { PageTitleComponent } from './page-title.component';
+import { Sidebar, SidebarContent, SidebarLayout } from './sidebar/sidebar.component';
 
 @NgModule({
     declarations: [
         CheckboxGroupComponent,
-        PageTitleComponent
+        PageTitleComponent,
+        Sidebar,
+        SidebarContent,
+        SidebarLayout
     ],
     imports: [
         CommonModule,
@@ -15,7 +19,10 @@ import { PageTitleComponent } from './page-title.component';
     ],
     exports: [
         CheckboxGroupComponent,
-        PageTitleComponent
+        PageTitleComponent,
+        Sidebar,
+        SidebarContent,
+        SidebarLayout
     ]
 })
 export class CommonComponentsModule {
