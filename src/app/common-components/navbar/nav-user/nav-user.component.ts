d28a50184771966ce44545dcad7fcf4b49863a58
Sybren Kuiper <sybkuiper@gmail.com>
2020-06-24 11:31:27 +0200

    navbar changes

--- src/app/common-components/navbar/nav-user/nav-user.component.ts
+++ src/app/common-components/navbar/nav-user/nav-user.component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
 import { AuthenticationService } from 'src/app/authentication/authentication.service';
 import { User } from 'src/app/users/user';
+import { MessageService } from '../../message-box/message.service';
+import { createMessage } from '../../message-box/message';
 
 @Component({
     selector: 'app-nav-user',
@@ -10,7 +12,16 @@ import { User } from 'src/app/users/user';
 export class NavUserComponent {
     currentUser: User = new User();
 
-    constructor(private authenticationService: AuthenticationService) {
+    constructor(private authenticationService: AuthenticationService, private messageService: MessageService) {
         authenticationService.getCurrentUser().subscribe(user => this.currentUser = user);
+    }
+
+    logout(){
+        if(localStorage.getItem("Authorization")){
+            localStorage.removeItem("Authorization");
+            localStorage.removeItem("currentUser");
+            this.currentUser = null;
+            this.messageService.setMessage(createMessage('success', "You are logged out"));
+        }
     }
 } 
