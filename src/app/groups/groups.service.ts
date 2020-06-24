c9406a20883b0b73994c5565814767955d794743
marco <m.groenhagen@st.hanze.nl>
2020-06-24 15:00:20 +0200

    fixed bug in group form component (hopefully)

--- src/app/groups/groups.service.ts
+++ src/app/groups/groups.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Group } from './group';
 import { of, Observable } from 'rxjs';
-import { groups} from './mock-groups';
 import { User } from '../users/user';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
@@ -14,10 +13,6 @@ export class GroupsService {
 
     getGroup(id: number): Observable<Group>{
         return this.http.get<Group>('groups/' + id).pipe(map(group => group));
-    }
-
-    getMemberOfGroup(group: number, id: number): Observable<Group>{
-        return of(groups[group][id]);
     }
 
     getGroups(): Observable<Group[]>{
@@ -41,11 +36,5 @@ export class GroupsService {
 
     getUserByName(username: string): Observable<User>{
         return this.http.get<User>('users/name/' + username);
-    }
-
-    joinGroupMembers(id: number){
-        let currentGroup = groups[id].group;
-        let members = currentGroup.join();
-        return members;
     }
 }
