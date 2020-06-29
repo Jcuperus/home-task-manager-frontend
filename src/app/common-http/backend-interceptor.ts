83fd96a630ce95109d8af561177e497d70a88079
marco <m.groenhagen@st.hanze.nl>
2020-06-29 11:23:22 +0200

    added button for groups list

--- src/app/common-http/backend-interceptor.ts
+++ src/app/common-http/backend-interceptor.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class BackendInterceptor implements HttpInterceptor {
     
-    backendUrl = "http://tasks.jaep.nl";
+    backendUrl = "http://localhost:8080";
     
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(request.clone({ url: `${this.backendUrl}/${request.url}` }));
