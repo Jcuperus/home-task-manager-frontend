3dc8bab7136595e2cd00e735cb8878f5e2fc76c0
Sybren Kuiper <sybkuiper@gmail.com>
2020-06-29 16:10:26 +0200

    navbar only shows links when logged in

--- src/app/common-components/navbar/navbar.component.ts
+++ src/app/common-components/navbar/navbar.component.ts
@@ -1,16 +1,29 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { AuthenticationService } from 'src/app/authentication/authentication.service';
 
 @Component({
     selector: 'app-navbar',
     styleUrls: ['./navbar.component.css'],
     templateUrl: './navbar.component.html'
 })
-export class NavbarComponent { 
+export class NavbarComponent implements OnInit{ 
     links = [
         { route: '/tasks', title: 'Tasks' }
     ];
 
+    loggedIn = false;
+
     mobileNavOpen = false;
+
+    constructor(private authenticationService: AuthenticationService) {  }
+
+    ngOnInit(){
+        this.authenticationService.getCurrentUser().subscribe(resp => {
+            if(resp){
+                this.loggedIn = true;
+            }
+        })
+    }
 
     toggleMobileNav() {
         this.mobileNavOpen = !this.mobileNavOpen;
